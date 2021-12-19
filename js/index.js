d8d145ed33d75247b0c2d96a86ced1137650d019
Jean <nguyen.jean@me.com>
2021-12-19 13:40:32 +0100

    upadte animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //import '../node_modules/@fortawesome/fontawesome-free/js/all';
+//import 'animate.css';
 
 const togglenav = document.querySelector('nav');
 const cancel = document.getElementById('cancel');
@@ -6,30 +7,58 @@ const chevron = document.getElementById('chevron');
 const home = document.getElementById('home');
 const header = document.querySelector('header');
 const footer = document.querySelector('footer');
-
+//4 Zones
+const aboutMe = document.getElementById('aboutMe');
+const portfolio = document.getElementById('portfolio');
+const contact = document.getElementById('contact');
 // cancel icon
 cancel.addEventListener('click', function () {
   home.style.display = 'flex';
   cancel.style.display = 'none';
   header.style.display = 'block';
   footer.style.display = 'none';
+  chevron.style.transform = 'rotate(0deg)';
 
   //à propos de moi
-  document.getElementById('aboutMe').classList.remove('animate__slideInDown');
-  document.getElementById('aboutMe').classList.add('animate__slideOutUp');
-  setTimeout(function () {
-    document.getElementById('aboutMe').style.display = 'none';
-  }, 1000);
+  if (cancel.classList.contains('cancel-aboutMe')) {
+    aboutMe.classList.remove('animate__slideInDown');
+    aboutMe.classList.add('animate__slideOutUp');
+    aboutMe.classList.add('animate__faster');
+    setTimeout(function () {
+      aboutMe.style.display = 'none';
+      home.classList.remove('animate__slideInUp');
+      cancel.classList.remove('cancel-aboutMe');
+      aboutMe.classList.remove('animate__slideOutUp');
+      aboutMe.classList.remove('animate__faster');
+    }, 1000);
+  }
 
   // portfolio
-  document
-    .getElementById('portfolio')
-    .classList.remove('animate__slideIntLeft');
-  document.getElementById('portfolio').classList.add('animate__slideOutLeft');
-  setTimeout(function () {
-    document.getElementById('portfolio').style.display = 'none';
-  }, 1000);
-  document.getElementById('contact').style.display = 'none';
+  if (cancel.classList.contains('cancel-portfolio')) {
+    portfolio.classList.remove('animate__slideInLeft');
+    portfolio.classList.add('animate__backOutLeft');
+    setTimeout(function () {
+      portfolio.style.display = 'none';
+      home.classList.remove('animate__slideInRight');
+      cancel.classList.remove('cancel-portfolio');
+      portfolio.classList.remove('animate__backOutLeft');
+    }, 1000);
+  }
+
+  // contact
+  if (cancel.classList.contains('cancel-contact')) {
+    contact.classList.remove('animate__slideInRight');
+    //contact.classList.add('animate__faster');
+    contact.classList.add('animate__slideOutRight');
+    setTimeout(function () {
+      contact.style.display = 'none';
+      home.classList.remove('animate__slideInLeft');
+      cancel.classList.remove('cancel-contact');
+      contact.classList.remove('animate__slideOutRight');
+      contact.classList.remove('animate__faster');
+    }, 1000);
+  }
+
   document.getElementById('skills').style.display = 'none';
 });
 
@@ -39,36 +68,33 @@ togglenav.addEventListener('click', function (e) {
   e.defaultPrevented;
   const cible = e.target;
 
-  function display() {
-    home.style.display = 'none';
-    cancel.style.display = 'block';
-    header.style.display = 'none';
-    footer.style.display = 'flex';
-  }
+  home.style.display = 'none';
+  cancel.style.display = 'block';
+  header.style.display = 'none';
+  footer.style.display = 'flex';
+  home.classList.remove('animate__fadeIn');
 
   if (cible.firstChild.nodeValue == 'À propos de moi') {
-    const aboutMe = document.getElementById('aboutMe');
+    cancel.classList.add('cancel-aboutMe');
     chevron.style.transform = 'rotate(270deg)';
     aboutMe.style.display = 'block';
-    aboutMe.classList.remove('animate__slideOutUp');
     aboutMe.classList.add('animate__slideInDown');
-    display();
+    home.classList.add('animate__slideInUp');
   }
   if (cible.firstChild.nodeValue == 'Portfolio') {
-    const element = document.getElementById('portfolio');
-    cancel.style.display = 'block';
+    cancel.classList.add('cancel-portfolio');
     chevron.style.transform = 'rotate(180deg)';
-    element.style.display = 'block';
-    element.classList.remove('animate__slideOutLeft');
-    element.classList.add('animate__slideInLeft');
-    display();
+    portfolio.style.display = 'block';
+    portfolio.classList.add('animate__slideInLeft');
+    home.classList.add('animate__slideInRight');
   }
   if (cible.firstChild.nodeValue == 'Contact') {
-    document.getElementById('contact').style.display = 'block';
-    display();
+    cancel.classList.add('cancel-contact');
+    contact.style.display = 'block';
+    contact.classList.add('animate__slideInRight');
+    home.classList.add('animate__slideInLeft');
   }
   if (cible.firstChild.nodeValue == 'Mes compétences') {
     document.getElementById('skills').style.display = 'block';
-    display();
   }
 });
